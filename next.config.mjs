cdeba35d50ee38eba37757adc02a18f2785695fb
arjunfrontend-dev <arjunfrontend@gmail.com>
2025-09-15 19:01:05 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  output: 'export',
+  basePath: '/<your-repo-name>',
+  images: {
+    unoptimized: true,
+  },
+};
+ 
+module.exports = nextConfig;
