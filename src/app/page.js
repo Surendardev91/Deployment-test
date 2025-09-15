bf45bb3fa948e5ee0bc53c9e0791296dff5eb852
Surendardev91 <131133375+Surendardev91@users.noreply.github.com>
2025-09-15 19:23:21 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className={styles.title}></h1>
+        <h1 className={styles.title}>
         deployment test....
         </h1>
       </main>
